ce453678e7bf9cf111feda8a6f2fde643315c1fc
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-11-11 16:57:36 +0100

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "linebreak-style": 0,
+    }
+};
