39afb15e4523aee62ab2db5c81362ed389918f42
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-11-17 18:16:19 +0100

    Added login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import LoginPage from './views/LoginPage';
 import './App.css';
 
 function App() {
@@ -8,6 +9,11 @@ function App() {
     <div className="">
       <BrowserRouter>
         <Navbar />
+        <div style={{ marginTop: '2rem' }}>
+          <Switch>
+            <Route path='/login' component={LoginPage} />
+          </Switch>
+        </div>
       </BrowserRouter>
     </div>
   );
---
+++ src/store/actions/authActions.js
@@ -0,0 +1,20 @@
+const url = 'http://localhost:5000/api/v1/auth';
+export const signIn = (credentials) => {
+    return (dispatch, getState) => {
+        const request = new Request(`${url}/signin`, {
+            method: 'POST',
+            body: credentials,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        console.log(request);
+        fetch(request)
+            .then((data) => {
+                dispatch({ type: 'LOGIN_SUCCESS', data });
+            }).catch((err) => {
+                dispatch({ type: 'LOGIN_FAILED', err });
+            });
+    }
+}
