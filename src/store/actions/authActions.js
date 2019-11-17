a9680a01c05b19f85de3ade3b6a0325c8c4e0e24
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-11-17 20:17:34 +0100

    Updated Login actions

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,7 +1,7 @@
 const url = 'http://localhost:5000/api/v1/auth';
 export const signIn = (credentials) => {
     return (dispatch, getState) => {
-        const request = new Request(`http://localhost:5000/api/v1/auth/signin`, {
+        const request = new Request(`${url}/signin`, {
             method: 'POST',
             body: JSON.stringify(credentials),
             headers: new Headers({
@@ -11,7 +11,7 @@ export const signIn = (credentials) => {
             })
         });
         console.log(request);
-        fetch(`http://localhost:5000/api/v1/auth/signin`, request)
+        fetch(`${url}/signin`, request)
             .then((data) => {
                 dispatch({ type: 'LOGIN_SUCCESS', data });
             }).catch((err) => {
