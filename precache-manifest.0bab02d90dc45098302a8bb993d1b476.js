cba618189578fa3e4ea96770024e75fe4f70633c
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-11-17 13:28:45 +0100

    Updates

---
+++ precache-manifest.0bab02d90dc45098302a8bb993d1b476.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5cdd89b0695724cf8dadbcefe535f4",
+    "url": "/DevC_Capstone_Project_Frontend/index.html"
+  },
+  {
+    "revision": "d2c8d308713f1e184569",
+    "url": "/DevC_Capstone_Project_Frontend/static/css/main.a43cc5b2.chunk.css"
+  },
+  {
+    "revision": "0ab42b1e6fddcfc41aaf",
+    "url": "/DevC_Capstone_Project_Frontend/static/js/2.3af24475.chunk.js"
+  },
+  {
+    "revision": "d2c8d308713f1e184569",
+    "url": "/DevC_Capstone_Project_Frontend/static/js/main.73a1ea79.chunk.js"
+  },
+  {
+    "revision": "df85d62e3bf6e1ae5793",
+    "url": "/DevC_Capstone_Project_Frontend/static/js/runtime-main.3b024942.js"
+  },
+  {
+    "revision": "e130d119682ecb567748343bf7f263b0",
+    "url": "/DevC_Capstone_Project_Frontend/static/media/FranklinGothic-Book.e130d119.TTF"
+  },
+  {
+    "revision": "082252176f1f4953cea2a7e5e9f300f4",
+    "url": "/DevC_Capstone_Project_Frontend/static/media/FranklinGothic-Demi.08225217.TTF"
+  },
+  {
+    "revision": "09aff291f163ebea6a3fc9e98ee3281d",
+    "url": "/DevC_Capstone_Project_Frontend/static/media/FranklinGothic-Medium.09aff291.ttf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/DevC_Capstone_Project_Frontend/precache-manifest.0bab02d90dc45098302a8bb993d1b476.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DevC_Capstone_Project_Frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
