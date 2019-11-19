914616b64ec8bfa590b4e4dfbf66999da868caa6
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-11-19 12:22:11 +0100

    Updates

---
+++ precache-manifest.df78886e409429432115cfbecacd27a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9aa63392413a2131ddcf40d6a7581d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1102d96030a9e032e73b",
+    "url": "/static/css/main.88bc7801.chunk.css"
+  },
+  {
+    "revision": "8ba6eb7ed13e5b5e5724",
+    "url": "/static/js/2.2196b779.chunk.js"
+  },
+  {
+    "revision": "1102d96030a9e032e73b",
+    "url": "/static/js/main.900fdb1d.chunk.js"
+  },
+  {
+    "revision": "ca1d9f7d038f31c4c734",
+    "url": "/static/js/runtime-main.ec65aa90.js"
+  },
+  {
+    "revision": "e130d119682ecb567748343bf7f263b0",
+    "url": "/static/media/FranklinGothic-Book.e130d119.TTF"
+  },
+  {
+    "revision": "082252176f1f4953cea2a7e5e9f300f4",
+    "url": "/static/media/FranklinGothic-Demi.08225217.TTF"
+  },
+  {
+    "revision": "09aff291f163ebea6a3fc9e98ee3281d",
+    "url": "/static/media/FranklinGothic-Medium.09aff291.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DevC_Capstone_Project_Frontend/precache-manifest.89eeed431bdb36b39fdfcca6a431e750.js"
+  "/precache-manifest.df78886e409429432115cfbecacd27a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DevC_Capstone_Project_Frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
