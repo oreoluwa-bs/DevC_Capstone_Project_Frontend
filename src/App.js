1fe0431d9bc1f11de057004010aad6067e6bfebf
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-11-19 05:52:42 +0100

    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, useRouteMatch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LoginPage from './views/LoginPage';
 import SignupPage from './views/SignupPage';
@@ -7,15 +7,16 @@ import Dashboard from './views/Dashboard';
 import './App.css';
 
 function App() {
+  const match = useRouteMatch();
   return (
     <div className="">
       <BrowserRouter>
         <Navbar />
         <div style={{ marginTop: '2rem' }}>
           <Switch>
-            <Route exact path='/' component={Dashboard} />
-            <Route path='/login' component={LoginPage} />
-            <Route path='/signup' component={SignupPage} />
+            <Route exact path={`${match.url}/`} component={Dashboard} />
+            <Route path={`${match.url}/login`}  component={LoginPage} />
+            <Route path={`${match.url}/signup`}  component={SignupPage} />
           </Switch>
         </div>
       </BrowserRouter>
