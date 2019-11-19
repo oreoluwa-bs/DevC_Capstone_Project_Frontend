801e91a387e1827437389fe6785bfcc012ebdb3b
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-11-19 12:30:43 +0100

    Updates

---
+++ precache-manifest.584d712f3d7cddd8d9c1e724ee337f15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902cf839e9b1f78ed83a66b6452c6f28",
+    "url": "/DevC_Capstone_Project_Frontend/index.html"
+  },
+  {
+    "revision": "fabdce2400581163f94a",
+    "url": "/DevC_Capstone_Project_Frontend/static/css/main.9b20184a.chunk.css"
+  },
+  {
+    "revision": "8ba6eb7ed13e5b5e5724",
+    "url": "/DevC_Capstone_Project_Frontend/static/js/2.2196b779.chunk.js"
+  },
+  {
+    "revision": "fabdce2400581163f94a",
+    "url": "/DevC_Capstone_Project_Frontend/static/js/main.1add2f13.chunk.js"
+  },
+  {
+    "revision": "df85d62e3bf6e1ae5793",
+    "url": "/DevC_Capstone_Project_Frontend/static/js/runtime-main.3b024942.js"
+  },
+  {
+    "revision": "e130d119682ecb567748343bf7f263b0",
+    "url": "/DevC_Capstone_Project_Frontend/static/media/FranklinGothic-Book.e130d119.TTF"
+  },
+  {
+    "revision": "082252176f1f4953cea2a7e5e9f300f4",
+    "url": "/DevC_Capstone_Project_Frontend/static/media/FranklinGothic-Demi.08225217.TTF"
+  },
+  {
+    "revision": "09aff291f163ebea6a3fc9e98ee3281d",
+    "url": "/DevC_Capstone_Project_Frontend/static/media/FranklinGothic-Medium.09aff291.ttf"
+  }
+]);
